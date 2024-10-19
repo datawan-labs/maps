c0b84dcebb9e8f679f19ca5433d8ca7b5738458a
Jafar Aziz <aziz@jafaraziz.com>
2024-10-19 23:46:44 +0700

    feat: setup project for cloudflare workers with static assets

---
+++ src/index.test.ts
@@ -0,0 +1,54 @@
+import worker from ".";
+import { describe, it, expect } from "vitest";
+import {
+  env,
+  SELF,
+  createExecutionContext,
+  waitOnExecutionContext,
+} from "cloudflare:test";
+
+describe("Hello World user worker", () => {
+  describe("request for /message", () => {
+    it('/ responds with "Hello, World!" (unit style)', async () => {
+      const request = new Request<unknown, IncomingRequestCfProperties>(
+        "http://example.com/message"
+      );
+      // Create an empty context to pass to `worker.fetch()`.
+      const ctx = createExecutionContext();
+      const response = await worker.fetch(request, env as Env, ctx);
+      // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+      await waitOnExecutionContext(ctx);
+      expect(await response.text()).toMatchInlineSnapshot(`"Hello, World!"`);
+    });
+
+    it('responds with "Hello, World!" (integration style)', async () => {
+      const request = new Request("http://example.com/message");
+      const response = await SELF.fetch(request);
+      expect(await response.text()).toMatchInlineSnapshot(`"Hello, World!"`);
+    });
+  });
+
+  describe("request for /random", () => {
+    it("/ responds with a random UUID (unit style)", async () => {
+      const request = new Request<unknown, IncomingRequestCfProperties>(
+        "http://example.com/random"
+      );
+      // Create an empty context to pass to `worker.fetch()`.
+      const ctx = createExecutionContext();
+      const response = await worker.fetch(request, env as Env, ctx);
+      // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+      await waitOnExecutionContext(ctx);
+      expect(await response.text()).toMatch(
+        /[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/
+      );
+    });
+
+    it("responds with a random UUID (integration style)", async () => {
+      const request = new Request("http://example.com/random");
+      const response = await SELF.fetch(request);
+      expect(await response.text()).toMatch(
+        /[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}/
+      );
+    });
+  });
+});
