0d895ca78585748425d0fc6f7838c6d2c51b1096
Jafar Aziz <aziz@jafaraziz.com>
2024-11-07 01:27:09 +0700

    chore: set antialias and update initial view

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,16 @@ const App = () => {
   return (
     <div className="absolute h-svh w-full">
       <MapLibre
+        antialias={true}
         attributionControl={false}
-        mapStyle="https://maps.datawan.id/styles/black.json"
+        mapStyle="https://maps.datawan.id/styles/light-3d.json"
+        initialViewState={{
+          zoom: 16,
+          pitch: 60,
+          bearing: 14,
+          latitude: -6.207102,
+          longitude: 106.822657,
+        }}
         style={{
           top: 0,
           left: 0,
